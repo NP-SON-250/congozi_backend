25e408411f837a108577660fc58a591df5ee6262
NP-SON-250 <hakizimanaalexis123@gmail.com>
2025-08-11 17:55:02 +0200

    change user model4

--- src/models/Congozi.users.model.js
+++ src/models/Congozi.users.model.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema({
   role: {
     type: String,
     enum: ["student", "admin", "supperAdmin", "school"],
-    default: "supperAdmin",
+    default: "student",
   },
   purchasedExams: [{ type: mongoose.Schema.ObjectId, ref: "exams" }],
   purchasedAccounts: [{ type: mongoose.Schema.ObjectId, ref: "accounts" }],
