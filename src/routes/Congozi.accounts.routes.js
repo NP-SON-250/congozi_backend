ac5e782aa9fe3f2f2706269678a47a6c13088383
NP-SON-250 <hakizimanaalexis123@gmail.com>
2025-08-13 18:03:29 +0200

    Change account route

--- src/routes/Congozi.accounts.routes.js
+++ src/routes/Congozi.accounts.routes.js
@@ -1,19 +1,18 @@
 import express from "express";
-import { 
+import {
   createAccount,
   updateAccount,
   getAllAccount,
   getAccountById,
-  deleteAccount
-
- } from "../controllers/Congozi.accounts.controllers";
+  deleteAccount,
+} from "../controllers/Congozi.accounts.controllers";
 import fileUpload from "../helper/multer";
-import { normal,admins,supperAdmins } from "../middleware/middleware";
+import { normal, admins, supperAdmins } from "../middleware/middleware";
 
 const accountRoute = express.Router();
-accountRoute.post("/", normal,fileUpload.single("title"), createAccount);
-accountRoute.put("/:id", normal,fileUpload.single("title"), updateAccount);
-accountRoute.delete("/:id",supperAdmins, deleteAccount);
+accountRoute.post("/", normal, fileUpload.single("title"), createAccount);
+accountRoute.put("/:id", normal, fileUpload.single("title"), updateAccount);
+accountRoute.delete("/:id", supperAdmins, deleteAccount);
 accountRoute.get("/", getAllAccount);
 accountRoute.get("/:id", getAccountById);
 
